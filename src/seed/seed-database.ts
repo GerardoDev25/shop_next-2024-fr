4b39a6ea0f0e06c1276f127783fec8e10d8c6064
Gerardo Miranda <gerardodev25@gmail.com>
2024-08-15 15:17:59 -0400

    feat(dev): :sparkles: database seed done

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,8 +1,48 @@
-import { Prisma } from '@prisma/client';
 import { initialData } from './seed';
+import prisma from '../lib/prisma';
 
 async function main() {
-  console.log(initialData);
+  // * delete all data from the database
+
+  await prisma.productImage.deleteMany();
+  await prisma.product.deleteMany();
+  await prisma.category.deleteMany();
+
+  const { categories, products } = initialData;
+
+  // * insert categories
+  await prisma.category.createMany({
+    data: categories.map((category) => ({ name: category })),
+  });
+
+  // * insert products
+  const categoriesDB = await prisma.category.findMany();
+  const categoriesMap = categoriesDB.reduce((map, category) => {
+    map[category.name.toLowerCase()] = category.id;
+    return map;
+  }, {} as Record<string, string>);
+
+  products.forEach(async (product) => {
+    const { type, images, ...rest } = product;
+
+    const dbProduct = await prisma.product.create({
+      data: {
+        ...rest,
+        categoryId: categoriesMap[type],
+      },
+    });
+
+    // Images
+    const imagesData = images.map((image) => ({
+      url: image,
+      productId: dbProduct.id,
+    }));
+
+    await prisma.productImage.createMany({
+      data: imagesData,
+    });
+  });
+
   console.log('executed successfully');
 }
 
