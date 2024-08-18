85fba506a90c608c699a54825e58844f062588fe
Gerardo Miranda <gerardodev25@gmail.com>
2024-08-18 18:28:23 -0400

    style(ui): :lipstick: give currency format to order sumary component

--- src/app/(shop)/cart/ui/OrderSummary.tsx
+++ src/app/(shop)/cart/ui/OrderSummary.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useCartStore } from '@/store/cart';
+import { currencyFormat } from '@/utils';
 import { useEffect, useState } from 'react';
 
 export const OrderSummary = () => {
@@ -26,13 +27,13 @@ export const OrderSummary = () => {
       </span>
 
       <span>Subtotal</span>
-      <span className='text-right'>${subTotal}</span>
+      <span className='text-right'>{currencyFormat(subTotal)}</span>
 
       <span>Tax(15%)</span>
-      <span className='text-right'>{tax}</span>
+      <span className='text-right'>{currencyFormat(tax)}</span>
 
       <span className='mt-5 text-2xl'>Total</span>
-      <span className='mt-5 text-2xl text-right'>${total}</span>
+      <span className='mt-5 text-2xl text-right'>{currencyFormat(total)}</span>
     </div>
   );
 };
