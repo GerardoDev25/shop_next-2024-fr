0a4661ff8ae98324a56e3e56c21909ebc1639c3f
Gerardo Miranda <gerardodev25@gmail.com>
2024-08-22 08:12:41 -0400

    chore(dev): :construction: create set user address action

---
+++ src/actions/address/set-user-address.ts
@@ -0,0 +1,46 @@
+import { Address } from '@/interfaces';
+import { prisma } from '@/lib';
+
+export const setUserAddress = async (address: Address, userId: string) => {
+  try {
+    const newAddress = await createORReplaceAddress(address, userId);
+
+    return {
+      ok: true,
+      address: newAddress,
+    };
+  } catch (error) {
+    return {
+      ok: false,
+      message: "couldn't record the data",
+    };
+  }
+};
+
+const createORReplaceAddress = async (address: Address, userId: string) => {
+  try {
+    const storedAddress = await prisma.userAddress.findUnique({
+      where: { userId },
+    });
+
+    const { country, ...rest } = address;
+    const addressToSave = { ...rest, userId, countryId: country };
+
+    if (!storedAddress) {
+      const newAddress = await prisma.userAddress.create({
+        data: addressToSave,
+      });
+      return newAddress;
+    }
+
+    const updatedAddress = await prisma.userAddress.update({
+      where: { userId },
+      data: addressToSave,
+    });
+
+    return updatedAddress;
+  } catch (error) {
+    console.log(error);
+    throw new Error('Error creating or replacing address');
+  }
+};
