7a0ed20ef7c08cde06bb9939b7658d57b969e242
Gerardo Miranda <gerardodev25@gmail.com>
2024-08-23 11:49:34 -0400

    chore(dev): :construction: create order and order items transaction

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -2,6 +2,7 @@
 
 import { auth } from '@/auth.config';
 import { Address, Size } from '@/interfaces';
+import { prisma } from '@/lib';
 
 interface ProductToOrder {
   productId: string;
@@ -9,8 +10,10 @@ interface ProductToOrder {
   size: Size;
 }
 
-export const placeOrder = async (props: ProductToOrder[], address: Address) => {
-  console.log({ props, address });
+export const placeOrder = async (
+  productIds: ProductToOrder[],
+  address: Address
+) => {
   const session = await auth();
   const userId = session?.user.id;
 
@@ -21,6 +24,69 @@ export const placeOrder = async (props: ProductToOrder[], address: Address) => {
     };
   }
 
-  // const { productId, quantity, size } = props;
-  console.log({ props, address, userId });
+  // * get product info
+  const products = await prisma.product.findMany({
+    where: { id: { in: productIds.map((p) => p.productId) } },
+  });
+
+  // * calculate amounts and headers
+  const itemsInOrder = productIds.reduce((count, p) => count + p.quantity, 0);
+
+  const { subTotal, tax, total } = productIds.reduce(
+    (totals, item) => {
+      const productQuantity = item.quantity;
+      const product = products.find((product) => product.id === item.productId);
+
+      if (!product) {
+        throw new Error(`Product not found: ${item.productId}`);
+      }
+
+      const subTotal = product.price * productQuantity;
+      totals.subTotal += subTotal;
+      totals.tax += subTotal * 0.15;
+      totals.total += subTotal * 1.15;
+
+      return totals;
+    },
+    { subTotal: 0, tax: 0, total: 0 }
+  );
+
+  // * create database transaction
+
+  const prismaTx = await prisma.$transaction(async (tx) => {
+    // * 1 update product stock
+    // * 2 create order and detail
+
+    const order = await tx.order.create({
+      data: {
+        userId,
+        itemsInOrder,
+        subTotal,
+        tax,
+        total,
+        isPaid: false,
+
+        OrderItems: {
+          createMany: {
+            data: productIds.map((p) => ({
+              productId: p.productId,
+              quantity: p.quantity,
+              size: p.size,
+              price:
+                products.find((product) => product.id === p.productId)?.price ??
+                0,
+            })),
+          },
+        },
+      },
+    });
+
+    // * 3 create order address
+
+    return {
+      order,
+    };
+  });
+
+  // console.log({ products, itemsInOrder, subTotal, tax, total });
 };
