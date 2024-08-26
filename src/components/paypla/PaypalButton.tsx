51244af60d8c0eb439253d071bb051f948596327
Gerardo Miranda <gerardodev25@gmail.com>
2024-08-26 09:58:35 -0400

    style(ui): :lipstick: add skeleton animation while load paypal buttons

--- src/components/paypla/PaypalButton.tsx
+++ src/components/paypla/PaypalButton.tsx
@@ -1,8 +1,19 @@
 'use client';
 
 import React from 'react';
-import { PayPalButtons } from '@paypal/react-paypal-js';
+import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js';
 
 export const PaypalButton = () => {
+  const [{ isPending }] = usePayPalScriptReducer();
+
+  if (isPending) {
+    return (
+      <div className='animate-pulse'>
+        <div className='h-11 bg-gray-300 rounded'></div>
+        <div className='mt-3 h-11 bg-gray-300 rounded'></div>
+      </div>
+    );
+  }
+
   return <PayPalButtons />;
 };
