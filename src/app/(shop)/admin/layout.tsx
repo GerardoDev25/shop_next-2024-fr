3f3f0706cf3281496883a54af712519d04cf89ad
Gerardo Miranda <gerardodev25@gmail.com>
2024-08-28 09:27:27 -0400

    chore(dev): :construction: protect admin routes

---
+++ src/app/(shop)/admin/layout.tsx
@@ -0,0 +1,15 @@
+import { auth } from '@/auth.config';
+import { redirect } from 'next/navigation';
+
+interface Props {
+  children: React.ReactNode;
+}
+export default async function AdminLayout({ children }: Props) {
+  const session = await auth();
+
+  if (session?.user.role !== 'admin') {
+    redirect('/');
+  }
+
+  return <>{children}</>;
+}
